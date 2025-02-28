0fe647e0159d8d555d97daf16124d72bfaffca40
pavell <pavellupan229@gmail.com>
2025-02-28 13:06:12 +0100

    el copilot se la ha pelado y lo ha hecho solo haha

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
+
+    static char[][] laberint = {{' ', '#', ' ', ' ', ' '}, {' ', '#', ' ', ' ', ' '}, {' ', ' ', ' ', '#', ' '}, {'#', '#', '#', '#', ' '}, {'X', ' ', ' ', ' ', ' '}};
     public static void main(String[] args) {
-        
+        imprimirLaberint(laberint);
+        resoldreLaberint(0, 0);
     }
     private static void imprimirLaberint(char[][] laberint) { // Mostra el laberint actual a la consola
         System.out.print("\033[H\033[2J"); // Esborra la pantalla per animació suau
@@ -21,6 +24,27 @@ public class Main {
         Thread.currentThread().interrupt();
         }
     }
-
-    public static boolean
+    private static boolean esValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < laberint.length && y < laberint[0].length && laberint[x][y] == ' ' && laberint[x][y] != '#';
+    }
+    public static boolean resoldreLaberint( int x, int y){ {
+        
+        if (esValid(x,y)){
+            if (laberint[x][y] != 'X'){
+                laberint[x][y] = '.';
+                imprimirLaberint(laberint);
+                esperar(150);
+                if (resoldreLaberint(x+1,y) || resoldreLaberint(x-1,y) || resoldreLaberint(x,y+1) || resoldreLaberint(x,y-1)){
+                    return true;
+                }
+                laberint[x][y] = ' ';
+                imprimirLaberint(laberint);
+                esperar(150);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 }
