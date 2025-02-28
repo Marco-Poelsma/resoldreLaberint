36c1f1c57424ec4b1494ba470433015febfac9f8
pavell <pavellupan229@gmail.com>
2025-02-28 13:27:43 +0100

    temps d'espera modificat

--- Main.java
+++ Main.java
@@ -44,7 +44,7 @@ public class Main {
                 return true;
             }
             laberint[x][y] = 'O';
-            esperar(150);
+            esperar(50);
             imprimirLaberint(laberint);
             if (laberint[x][y] == 'X') {
                 return true;
